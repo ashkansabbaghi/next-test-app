0379afc599f9df0fdfa8e76aa983ce739b0c9364
ashkan73 <ashkansabbaghi73@gmail.com>
2023-10-18 11:38:43 +0330

    commit change first

---
+++ src/app/pages/index.tsx
@@ -0,0 +1,6 @@
+export default function index() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            ashkan page 
+        </main>)
+}
